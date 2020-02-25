3b344a2dd3bfab3c3b45a2dbc6be06ff92480ae5
Michael D'Attilo <anuncertaintruth@gmail.com>
2020-02-25 12:58:51 -0500

    Heroku Setup
    Added package.json, configs, and procfile as required for a Heroku deployment.

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 const config = require('./config/config.json');
-const hostname = config.hostname;
+//const hostname = config.hostname;
 const port = config.port;
 
 const server = require('./controller.js');
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running on Port:${port}/`);
 });
 
