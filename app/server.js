29bb72f3b0805de144787f1425b8d0734c80b706
Michael D'Attilo <anuncertaintruth@gmail.com>
2020-02-25 13:15:15 -0500

    Heroku port settings
    Adjusted heroku build to read port from Heroku's environment variable.

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
 const config = require('./config/config.json');
-//const hostname = config.hostname;
-const port = config.port;
+const hostname = config.hostname;
+const port = process.env.PORT|| config.port;
 
 const server = require('./controller.js');
 
-server.listen(port, () => {
-    console.log(`Server running on Port:${port}/`);
+server.listen(port, hostname, () => {
+    console.log(`Server running at ${hostname}:${port}/`);
 });
 
